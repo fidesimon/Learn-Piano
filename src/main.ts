cb363c136fe6840e75f9a0488510ff388b84f81b
Szymon S <fide.simon@gmail.com>
2018-02-04 23:41:33 +0000

    Code rafactored and fixed issue with missing note

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Notes } from './piano';
-import { HandlerFunctionality } from './piano';
+import { Piano } from './piano';
 
 if (environment.production) {
   enableProdMode();
@@ -16,7 +15,8 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 (function(){
   document.addEventListener("DOMContentLoaded", function(){
-    var note = new Notes();
-    var hand = new HandlerFunctionality(note);
+    //var note = new Notes();
+    //var hand = new HandlerFunctionality(note);
+    var piano = new Piano();
   });
 })();
--- src/piano.ts
+++ src/piano.ts
@@ -1,148 +1,225 @@
 import { WebMidi } from "./../node_modules/@types/webmidi/index";
 
-export class Notes {
-    private _notesString: string;
-    public NotesString: string;
-    public NotesArrayMIDI: any;
-    private _flatsString: string;
-    private _sharpsString: string;
-    private _emptyLine: string;
-    private _clef: string;
-    private _autoDisplay: boolean;
-    private _refreshInterval: number;
-    public CurrentNote: string;
-
-    constructor() {
-        this.init();
-    }
-    private init() {
-        this._notesString = "`abcdefghijklmn";
-        this.NotesString = "`abcdefghijklmn";
-        this.NotesArrayMIDI = [36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60];
-        this._flatsString = "àáãâäåæçèéêëìíî"; //to be used to display flat symbol
-        this._sharpsString = "ÐÑÓÒÔÕÖ×ØÙÚÛÜÝÞ"; //to be used to display flat symbol 
-        this._emptyLine = "=";
-        this._clef = "¯"; //¯-bass clef     &-treble clef
-        this._autoDisplay = false;
-        this._refreshInterval = 1000;
-    }
-
-    SetDisplayType(auto: boolean): void {
-        this._autoDisplay = auto;
-    }
-
-    public ChangeClef(clef: string): void {
-        if (clef.toLowerCase() === "bass") {
-            this._clef = "¯";
-            this.NotesArrayMIDI = [36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60];
-        }
-        else if (clef.toLowerCase() === "treble") {
-            this._clef = "&";
-            this.NotesArrayMIDI = [57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81];
-        }
-    }
-
-    private generateRandomNote(): string {
-        var newNote = this._notesString[(Math.floor(Math.random() * (this._notesString.length)))];
-        if(this.CurrentNote === newNote)
-            this.generateRandomNote();
-        else {
-            this.CurrentNote = newNote;
-        }
-        return newNote;
-    }
-
-    public GetNoteString(): string {
-        return this._clef + "===" + this.generateRandomNote() + "====";
-    }
+export enum ClefEnum{
+    Bass = "¯",
+    Treble = "&"
 }
 
-export class HandlerFunctionality {
-    private _notesObject: Notes;
-    private _intervalVal: number;
+export interface IPianoEventHandling{
+    ClefChange(): void;
+    AutoPlayChange() : void;
+    IntervalChange() : void;
+    ManualNoteChange(e : any) : void;
+}
 
-    constructor(notesObject: Notes) {
-        this._notesObject = notesObject;
-        this.renderNote();
+export interface IPracticeSettings{
+    Clef: ClefEnum;
+    AutoPlay: boolean;
+    Interval: number;
+    MidiEnabled: boolean;
+}
+
+export class PracticeSettings implements IPracticeSettings{
+    Clef: ClefEnum;
+    AutoPlay: boolean;
+    Interval: number;
+    MidiEnabled: boolean;
+
+    constructor() {
         this.initialize();
     }
 
-    initialize() {
-        var xx = new MidiHandlers();
-        xx.init(this._notesObject);
+    initialize(){
+        this.Clef = ClefEnum.Bass;
+        this.AutoPlay = false;
+        this.Interval = 1000;
+    }
+}
 
-        document.addEventListener('keydown', this.handler.bind(this));
+export class PianoEventHandlers implements IPianoEventHandling{
+    private _practice: Practice;
+    private _intervalVal: number;
+
+    constructor(practice: Practice){
+        this._practice = practice;
+        this.initialize();
+    }
+
+    initialize(){
+        this._registerEventHandlers();
+        this.ClefChange();
+    }
+
+    private _registerEventHandlers(){
+        document.addEventListener('keydown', this.ManualNoteChange.bind(this));
         var clefDropDown = document.getElementById("clefDropDown");
-        clefDropDown.addEventListener('change', this.changeClef.bind(this));
+        clefDropDown.addEventListener('change', this.ClefChange.bind(this));
         var autoCheckBox = document.getElementById("autoCheckBox");
-        autoCheckBox.addEventListener('change', this.autoTrigger.bind(this));
+        autoCheckBox.addEventListener('change', this.AutoPlayChange.bind(this));
         var intervalTextBox = document.getElementById("intervalTextBox");
-        intervalTextBox.addEventListener('change', this.updateInterval.bind(this));
+        intervalTextBox.addEventListener('change', this.IntervalChange.bind(this));
     }
 
-    updateInterval() {
-        var intervalTextBox = <HTMLInputElement>document.getElementById("intervalTextBox");
-        var interval = +(intervalTextBox.value);
-        if (this._intervalVal !== null) { //means that interval is executing
-            this.stopInterval();
-            this.startInterval(interval);
+    ClefChange(){
+        var selectedClef = (<HTMLInputElement>document.getElementById("clefDropDown")).value;
+        if(selectedClef == "bass"){
+            this._practice.Settings.Clef = ClefEnum.Bass;
+            this._practice.NotesMap = {
+                "`": 36,
+                "à`": 35,
+                "Ð`": 37,
+                "a": 38,
+                "áa": 37,
+                "Ña": 39,
+                "b": 40,
+                "âb": 39,
+                "Òb": 41,
+                "c": 41,
+                "ãc": 40,
+                "Óc": 42,
+                "d": 43,
+                "äd": 42,
+                "Ôd": 44,
+                "e": 45,
+                "åe": 44,
+                "Õe": 46,
+                "f": 47,
+                "æf": 46,
+                "Öf": 48,
+                "g": 48,
+                "çg": 47,
+                "×g": 49,
+                "h": 50,
+                "èh": 49,
+                "Øh": 51,
+                "i": 52,
+                "éi": 51,
+                "Ùi": 53,
+                "j": 53,
+                "êj": 52,
+                "Új": 54,
+                "k": 55,
+                "ëk": 54,
+                "Ûk": 56,
+                "l": 57,
+                "ìl": 56,
+                "Ül": 58,
+                "m": 59,
+                "ím": 58,
+                "Ým": 60,
+                "n": 60,
+                "în": 59,
+                "Þn": 61
+            };
         }
+        else
+        {
+            this._practice.Settings.Clef = ClefEnum.Treble;
+            this._practice.NotesMap = {
+                "`": 57,
+                "à`": 56,
+                "Ð`": 58,
+                "a": 59,
+                "áa": 58,
+                "Ña": 60,
+                "b": 60,
+                "âb": 59,
+                "Òb": 61,
+                "c": 62,
+                "ãc": 61,
+                "Óc": 63,
+                "d": 64,
+                "äd": 63,
+                "Ôd": 65,
+                "e": 65,
+                "åe": 64,
+                "Õe": 66,
+                "f": 67,
+                "æf": 66,
+                "Öf": 68,
+                "g": 69,
+                "çg": 68,
+                "×g": 70,
+                "h": 71,
+                "èh": 70,
+                "Øh": 72,
+                "i": 72,
+                "éi": 71,
+                "Ùi": 73,
+                "j": 74,
+                "êj": 73,
+                "Új": 75,
+                "k": 76,
+                "ëk": 75,
+                "Ûk": 77,
+                "l": 77,
+                "ìl": 76,
+                "Ül": 78,
+                "m": 79,
+                "ím": 78,
+                "Ým": 80,
+                "n": 81,
+                "în": 80,
+                "Þn": 82
+            };
+        }
+        this._practice.RenderStaff();
+        document.getElementById("clefDropDown").blur();
     }
 
-    renderNote() {
-        document.getElementById("notesDiv").innerText = this._notesObject.GetNoteString();
-    }
-
-    startInterval(interval: number) {
-        this._intervalVal = window.setInterval(this.renderNote.bind(this), interval);
-    }
-
-    stopInterval() {
-        window.clearInterval(this._intervalVal);
-        this._intervalVal = null;
-    }
-
-    autoTrigger() {
+    AutoPlayChange(){
         var autoDisplay = <HTMLInputElement>document.getElementById("autoCheckBox");
-        this._notesObject.SetDisplayType(autoDisplay.checked);
+        this._practice.Settings.AutoPlay = autoDisplay.checked;
         var intervalTextBox = <HTMLInputElement>document.getElementById("intervalTextBox");
 
         if (autoDisplay.checked) {
             var intervalValue = +(intervalTextBox.value); // +(string) -> TypeScript converts string to number
             intervalTextBox.disabled = false;
-            this.startInterval(intervalValue);
+            this._startInterval(intervalValue);
         } else {
             intervalTextBox.disabled = true;
-            this.stopInterval();
+            this._stopInterval();
         }
+        document.getElementById("autoCheckBox").blur();
     }
 
-    changeClef() {
-        var selectedClef = (<HTMLInputElement>document.getElementById("clefDropDown")).value;
-        this._notesObject.ChangeClef(selectedClef);
-        this.renderNote();
+    IntervalChange(){
+        var intervalTextBox = <HTMLInputElement>document.getElementById("intervalTextBox");
+        var interval = +(intervalTextBox.value);
+        if (this._intervalVal !== null) { //means that interval is executing
+            this._stopInterval();
+            this._startInterval(interval);
+        }
+        document.getElementById("intervalTextBox").blur();
     }
 
-    handler(e) {
+    ManualNoteChange(e){
         var key = window.event ? e.keyCode : e.which;
-        if (key == 32) {
-            var el = document.getElementById("notesDiv");
-            el.innerText = this._notesObject.GetNoteString();
-        }
+        this._practice.RenderStaff();
+    }
+
+    private _startInterval(interval: number) {
+        this._intervalVal = window.setInterval(this._practice.RenderStaff.bind(this._practice), interval);
+    }
+
+    private _stopInterval() {
+        window.clearInterval(this._intervalVal);
+        this._intervalVal = null;
     }
 }
+
 export interface MIDINavigator extends Navigator {
     requestMIDIAccess(options?: WebMidi.MIDIOptions): Promise<WebMidi.MIDIAccess>;
 }
-export class MidiHandlers {
+
+export class MidiComponent{
     context = null;   // the Web Audio "context" object
     midiAccess = null;  // the MIDIAccess object.
     activeNotes = []; // the stack of actively-pressed keys
-    private _notesObject: Notes;
+    private _practice: Practice;
 
-    init(obj) {
-        //window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        this._notesObject = obj;//new Notes();
+    constructor(practice: Practice){
+        this._practice = practice;
         this.context = new AudioContext();
 
         if ((<MIDINavigator>navigator).requestMIDIAccess)
@@ -151,11 +228,9 @@ export class MidiHandlers {
             alert("No MIDI support present in your browser. You're gonna have a bad time.");
     }
 
-    renderNote(note) {
-        var index = this._notesObject.NotesString.indexOf(this._notesObject.CurrentNote);
-
-        if (index === this._notesObject.NotesArrayMIDI.indexOf(note)) {
-            document.getElementById("notesDiv").innerText = this._notesObject.GetNoteString();
+    private _checkNote(note) {
+        if(this._practice.NotesMap[this._practice.CurrentNote] === note){
+            this._practice.RenderStaff();
         }
     }
 
@@ -192,7 +267,7 @@ export class MidiHandlers {
     }
 
     noteOn(noteNumber) {
-        this.renderNote(noteNumber);
+        this._checkNote(noteNumber);
         this.activeNotes.push(noteNumber);
     }
 
@@ -201,5 +276,51 @@ export class MidiHandlers {
         if (position != -1) {
             this.activeNotes.splice(position, 1);
         }
+    }
+}
+
+export class Piano{
+    _practice: Practice;
+    _eventHandlers: PianoEventHandlers;
+    _midiComponent: MidiComponent;
+
+    constructor(){
+        this._practice = new Practice();
+        this._eventHandlers = new PianoEventHandlers(this._practice);
+        this._midiComponent = new MidiComponent(this._practice);
+    }
+}
+
+export class Practice{
+    public CurrentNote: string;
+    public NotesMap: {};
+    public Settings: PracticeSettings;
+    private _notesString = "`abcdefghijklmn";
+    private _flatsString = "àáâãäåæçèéêëìíî"; 
+    private _sharpsString = "ÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞ";
+    private _emptyLine = "=";
+
+    constructor(){
+        this.Settings = new PracticeSettings();
+        this.RenderStaff();
+    }
+
+    public RenderStaff(){
+        document.getElementById("notesDiv").innerText = this.getNoteString();
+    }
+    
+    private generateRandomNote(): string {
+        return this._notesString[(Math.floor(Math.random() * (this._notesString.length)))];
+    }
+
+    private getNoteString(): string {
+        var newNote = this.CurrentNote;
+
+        while(this.CurrentNote == newNote){
+            newNote = this.generateRandomNote();
+        }
+
+        this.CurrentNote = newNote;
+        return this.Settings.Clef + "===" + newNote + "====";
     }
 }
